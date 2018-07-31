47cf1d244ba6cdf20d50c51dedc054b23abd0169
Lvbo163 <lv.bo163@zte.com.cn>
2018-07-31 09:56:34 +0800

    init msb2pilot project
    init project by beego
    
    Issue-ID: MSB-230
    
    Change-Id: Iaf3afb1f8d28e54d61c0bd401737d2604149ff0b
    Signed-off-by: Lvbo163 <lv.bo163@zte.com.cn>

---
+++ msb2pilot/src/msb2pilot/controllers/default.go
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2018 ZTE Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     ZTE - initial Project
+ */
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
---
+++ msb2pilot/src/msb2pilot/main.go
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2018 ZTE Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     ZTE - initial Project
+ */
+package main
+
+import (
+	_ "msb2pilot/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
